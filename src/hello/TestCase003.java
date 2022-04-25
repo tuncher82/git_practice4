8df019f6589ef3a7b6a37a3483f0d46edc9f6d03
tuncherdurmush <tuncherdurmush@gmail.com>
2022-04-25 10:19:26 +0100

    This is the change from local IntelliJ

--- src/hello/TestCase003.java
+++ src/hello/TestCase003.java
@@ -8,10 +8,10 @@ public class TestCase003 {
     // when we merge and conflict will happen
 
 
-
     // awesome code
 
     // code finished in feature branch
     // this is my final result
 
 }
+// This is the change from local IntelliJ
